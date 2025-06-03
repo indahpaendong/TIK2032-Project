d7fe5a62d5a172ca1f559418c8b9e0a40feda19a
indahpaendong <indahpaendong026@student.unsrat.ac.id>
2025-06-03 19:33:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -52,11 +52,21 @@
     <hr>
 
     <!-- Contact -->
-    <section id="contact">
-        <h2>Contact</h2>
-        <p>Email: indahpaendong2005@email.com</p>
-        <p>Instagram : Indahprsla_</p>
-    </section>
+   <section id="contact">
+    <h2>Contact</h2>
+    <form action="simpan_contact.php" method="POST">
+        <label>Nama:</label><br>
+        <input type="text" name="nama" required><br><br>
+
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <label>Pesan:</label><br>
+        <textarea name="pesan" rows="4" required></textarea><br><br>
+
+        <button type="submit">Kirim</button>
+    </form>
+</section>
 
     <script src="script.js" defer></script>
 </body>
